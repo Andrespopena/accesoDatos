6b98adfb1ce4e7e047d181681acebe4e049b95f2
Andrés Peña <andres.penaortega06@gmail.com>
2021-03-16 15:11:52 +0100

    V1.1

--- accesoDatos/src/leerContactos/documento.java
+++ accesoDatos/src/leerContactos/documento.java
@@ -1,44 +1,58 @@
 package leerContactos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class documento {
-	//Para usar en los metodos
-	String SEPARATOR=";";
-	//atributps de la clase.
+	// Para usar en los metodos
+	String SEPARATOR = ";";
+	// atributps de la clase.
 	ArrayList<String> Nombre = new ArrayList<String>();
 	ArrayList<String> Apellidos = new ArrayList<String>();
 	ArrayList<String> tlf = new ArrayList<String>();
 	ArrayList<String> correoElectronico = new ArrayList<String>();
-	//Constructores
+
+	// Constructores
 	public documento() {
 	}
-	//metodo principal
-	public void LADatos(String line) {
+
+	// metodo principal
+	public void LADatos(FileReader archivo) throws IOException {
+		BufferedReader br = null;
 		try {
-			String[] linea = line.split(SEPARATOR);
-			Nombre.add(linea[0]);
-			Apellidos.add(linea[1]);
-			tlf.add(linea[2]);
-			correoElectronico.add(linea[3]);
-			
+			br = new BufferedReader(archivo);
+			String line = br.readLine();
+			while (null != line) {
+				String[] linea = line.split(SEPARATOR);
+				Nombre.add(linea[0]);
+				Apellidos.add(linea[1]);
+				tlf.add(linea[2]);
+				correoElectronico.add(linea[3]);
+				line = br.readLine();
+			}
+
 		} catch (Exception e) {
 			System.err.println("Alguno de los datos es incorrecto");
+		} finally {
+			if (null != br) {
+				br.close();
+			}
+
 		}
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		String palabra = new String();
 		palabra = "";
 		for (int i = 0; i < Nombre.size(); i++) {
-			palabra += "Contacto ["+(i+1)+"] { Nombre: "+Nombre.get(i)+", Apellido: "+Apellidos.get(i)+", Tlf: "+tlf.get(i)+", Correo: "+correoElectronico.get(i)+"}."+System.getProperty("line.separator");
+			palabra += "Contacto [" + (i + 1) + "] { Nombre: " + Nombre.get(i) + ", Apellido: " + Apellidos.get(i)
+					+ ", Tlf: " + tlf.get(i) + ", Correo: " + correoElectronico.get(i) + "}."
+					+ System.getProperty("line.separator");
 		}
 		return palabra;
 	}
-	
-	
-	
-	
+
 }
--- accesoDatos/src/leerContactos/lecturaDatos.java
+++ accesoDatos/src/leerContactos/lecturaDatos.java
@@ -1,30 +1,17 @@
 package leerContactos;
-
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class lecturaDatos {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = null;
+	public static void main(String[] args) {
+		
 		documento doc = new documento();
 		try {
 			FileReader archivo = new FileReader("C:\\Users\\Andrés\\OneDrive\\Escritorio\\contactos.csv");
-			br = new BufferedReader(archivo);
-			String line = br.readLine();
-			while (null != line) {
-				doc.LADatos(line);
-				line = br.readLine();
-			}
+			doc.LADatos(archivo);
 		} catch (Exception e) {
 			System.err.println("Documento no encontrado");
-		} finally {
-			if (null != br) {
-				br.close();
-			}
-
-		}
+		} 
 		
 		System.out.println(doc.toString());
 	}
