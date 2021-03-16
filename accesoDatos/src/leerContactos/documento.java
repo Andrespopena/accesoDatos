0674b10d2dc0dca9ba991f8ef049844d945c26dd
Andrés Peña <andres.penaortega06@gmail.com>
2021-03-16 15:35:24 +0100

    v1.2

--- accesoDatos/src/leerContactos/documento.java
+++ accesoDatos/src/leerContactos/documento.java
@@ -5,21 +5,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class documento {
-	// Para usar en los metodos
+public final class documento {
+	static // Para usar en los metodos
 	String SEPARATOR = ";";
 	// atributps de la clase.
-	ArrayList<String> Nombre = new ArrayList<String>();
-	ArrayList<String> Apellidos = new ArrayList<String>();
-	ArrayList<String> tlf = new ArrayList<String>();
-	ArrayList<String> correoElectronico = new ArrayList<String>();
+	static ArrayList<String> Nombre = new ArrayList<String>();
+	static ArrayList<String> Apellidos = new ArrayList<String>();
+	static ArrayList<String> tlf = new ArrayList<String>();
+	static ArrayList<String> correoElectronico = new ArrayList<String>();
+	
+	static String palabra = new String();
 
 	// Constructores
-	public documento() {
-	}
 
 	// metodo principal
-	public void LADatos(FileReader archivo) throws IOException {
+	public static void LADatos(FileReader archivo) throws IOException {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(archivo);
@@ -32,7 +32,6 @@ public class documento {
 				correoElectronico.add(linea[3]);
 				line = br.readLine();
 			}
-
 		} catch (Exception e) {
 			System.err.println("Alguno de los datos es incorrecto");
 		} finally {
@@ -43,9 +42,7 @@ public class documento {
 		}
 	}
 
-	@Override
-	public String toString() {
-		String palabra = new String();
+	public static String toText() {
 		palabra = "";
 		for (int i = 0; i < Nombre.size(); i++) {
 			palabra += "Contacto [" + (i + 1) + "] { Nombre: " + Nombre.get(i) + ", Apellido: " + Apellidos.get(i)
--- accesoDatos/src/leerContactos/lecturaDatos.java
+++ accesoDatos/src/leerContactos/lecturaDatos.java
@@ -4,16 +4,14 @@ import java.io.FileReader;
 public class lecturaDatos {
 
 	public static void main(String[] args) {
-		
-		documento doc = new documento();
 		try {
 			FileReader archivo = new FileReader("C:\\Users\\Andrés\\OneDrive\\Escritorio\\contactos.csv");
-			doc.LADatos(archivo);
+			documento.LADatos(archivo);
 		} catch (Exception e) {
 			System.err.println("Documento no encontrado");
 		} 
 		
-		System.out.println(doc.toString());
+		System.out.println(documento.toText());
 	}
 
 }
